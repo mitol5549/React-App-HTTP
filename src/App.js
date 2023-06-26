477f5afb835bf366026c4415fb1aa6fca72e2d7c
Andrey Osipov <mitol5549@gmail.com>
2023-06-26 12:31:57 +0200

    Loading State added.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import './App.css';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
+
     const response = await fetch('https://swapi.dev/api/films/');
 
     const data = await response.json();
@@ -21,6 +24,8 @@ const App = () => {
     });
 
     setMovies(transformedMovies);
+
+    setIsLoading(false);
   }
 
   return (
@@ -29,7 +34,9 @@ const App = () => {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </>
   );
